421e3c3156ef0c30ee8285431e828911d31fb9ce
Thanakrit Ritdej <thanakrit.r@arise.tech>
2024-05-17 14:41:35 +0700

    level1 TC1

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,5 +1,16 @@
 package main
 
+import "strconv"
+
 func FizzBuzz(n int) string {
-	return ""
+
+	if n%15 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
+		return "Fizz"
+	} else if n%5 == 0 {
+		return "Buzz"
+	}
+
+	return strconv.Itoa(n)
 }
