6abe1718fc5b37c1b68ca0747128b4657945b57f
Thanakrit Ritdej <thanakrit.r@arise.tech>
2024-05-17 12:55:51 +0700

    taxi fare

--- testjs/src/taxi.test.ts
+++ testjs/src/taxi.test.ts
@@ -1,8 +1,39 @@
-import {Taxi} from './taxi'
+import {Taxi, adjustFare} from './taxi'
 
 describe('Taxi', () => {
-  it('example test', () => {
-    const result = Taxi()
-    expect(result).toEqual(55)
+  it('dist: 5,  waitTime: 5', () => {
+    const result = Taxi(5, 5)
+    expect(result).toEqual(25)
+  }),
+  it('dist: 5.4, waitTime: 5', () => {
+    const result = Taxi(5.4, 5)
+    expect(result).toEqual(27)
+  }),
+  it('dist: 5.4, waitTime: 2.49', () => {
+    const result = Taxi(5.4, 2.49)
+    expect(result).toEqual(25)
+  }),
+  it('dist: 4.7, waitTime: 2.49', () => {
+    const result = Taxi(4.7, 2.49)
+    expect(result).toEqual(23)
+  }),
+  it('dist: 5,  waitTime: 4.8', () => {
+    const result = Taxi(5, 4.8)
+    expect(result).toEqual(25)
+  }),
+  it('dist: 5,  waitTime: 1.51', () => {
+    const result = Taxi(5, 1.51)
+    expect(result).toEqual(22)
   })
 })
+
+describe('Min afre', () => [
+  it('below minfare', () => {
+    const result = adjustFare(Taxi(5, 5))
+    expect(result).toEqual(35)
+  }),
+  it('more than minfare', () => {
+    const result = adjustFare(Taxi(8, 4))
+    expect(result).toEqual(36)
+  })
+])
--- testjs/src/taxi.ts
+++ testjs/src/taxi.ts
@@ -1,1 +1,8 @@
-export const Taxi = () => 55
+export const Taxi = (distance: any, waitTime : any) => {
+    const fare = 4*(Math.ceil(distance*2)/2) + Math.ceil(waitTime);
+    return fare;
+}
+
+export const adjustFare = (f: number) => {
+    return Math.max(f, 35);
+}
