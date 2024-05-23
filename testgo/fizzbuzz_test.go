75c68b351a75b9d2191507676a98155b7a63aaec
Thanakrit Ritdej <thanakrit.r@arise.tech>
2024-05-23 18:20:18 +0700

    level4 TC10

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -101,16 +101,16 @@ func TestFizzBuzzShouldReturnFizzWhenInput9(t *testing.T) {
 	}
 }
 
-// func TestFizzBuzzShouldReturnBuzzWhenInput10(t *testing.T) {
-// 	input := 10
+func TestFizzBuzzShouldReturnBuzzWhenInput10(t *testing.T) {
+	input := 10
 
-// 	got := FizzBuzz(input)
+	got := FizzBuzz(input)
 
-// 	want := "Buzz"
-// 	if got != want {
-// 		t.Errorf("got %q but want %q", got, want)
-// 	}
-// }
+	want := "Buzz"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
 
 // func TestFizzBuzzShouldReturn11WhenInput11(t *testing.T) {
 // 	input := 11
